b650ca64eff8aa9a844a28a3d4e64fa1141e217f
Zach Fedor <fedor@towermarketing.net>
2014-11-07 14:03:12 -0500

    removed evil, added minor tweaks to init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -19,6 +19,12 @@
 ;; enable transient mark mode
 (transient-mark-mode 1)
 
+;; enable emacsserver
+(server-mode 1)
+
+;; enable C-n movement to add new lines
+(setq next-line-add-newlines t)
+
 ;;;; ORG MODE
 ;; enable org-mode
 (require 'org)
@@ -27,36 +33,40 @@
 ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 ;; ^ is default in recent emacsen
 
+;; creating org workflow states
+(setq org-todo-keywords
+      '((sequence "TODO" "NEXT" "HOLD" "DONE")))
+
 ;; david o'toole's org tutorial configuration
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
 ;;;; EVIL MODE
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
+;;(add-to-list 'load-path "~/.emacs.d/evil")
+;;(require 'evil)
+;;(evil-mode 1)
 
 ;; evil mode key bindings
 
 ;; -- BEGIN -> set jk to ESC
-(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+;;(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
 
-(evil-define-command cofi/maybe-exit ()
-  :repeat change
-  (interactive)
-  (let ((modified (buffer-modified-p)))
-    (insert "j")
-    (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
-                           nil 0.5)))
-      (cond
-       ((null evt) (message ""))
-       ((and (integerp evt) (char-equal evt ?k))
-        (delete-char -1)
-        (set-buffer-modified-p modified)
-        (push 'escape unread-command-events))
-       (t (setq unread-command-events (append unread-command-events
-                                              (list evt))))))))
+;;(evil-define-command cofi/maybe-exit ()
+;;  :repeat change
+;;  (interactive)
+;;  (let ((modified (buffer-modified-p)))
+;;    (insert "j")
+;;    (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
+;;                           nil 0.5)))
+;;      (cond
+;;       ((null evt) (message ""))
+;;       ((and (integerp evt) (char-equal evt ?k))
+;;        (delete-char -1)
+;;        (set-buffer-modified-p modified)
+;;        (push 'escape unread-command-events))
+;;       (t (setq unread-command-events (append unread-command-events
+;;                                              (list evt))))))))
 ;; from: http://zuttobenkyou.wordpress.com/2011/02/15/some-thoughts-on-emacs-and-vim/
 ;; -- END --> set jk to ESC
 
