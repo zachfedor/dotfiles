a31b960afa7ac4c1a30b96f54f16f55b297f617f
Zach Fedor <fedor@towermarketing.net>
2014-11-13 08:56:20 -0500

    init.el - use C-x k to close emacsclient

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,6 +22,14 @@
 ;; enable emacsserver
 (server-mode 1)
 
+;; enable C-x k to close emacsclient
+(add-hook 'server-switch-hook
+          (lambda ()
+            (when (current-local-map)
+              (use-local-map (copy-keymap (current-local-map))))
+            (when server-buffer-clients
+              (local-set-key (kbd "C-x k") 'server-edit))))
+
 ;; enable C-n movement to add new lines
 (setq next-line-add-newlines t)
 
