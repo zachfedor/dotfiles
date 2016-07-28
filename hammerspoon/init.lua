711d7869b88e62640369898853a01249bf32bb9b
Zach Fedor <zachfedor@gmail.com>
2016-07-28 11:42:03 -0400

    adds break timer and cancel timer functions to Pomodoro

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -15,6 +15,11 @@ hs.hotkey.bind({"cmd", "ctrl", "shift"}, "W", function()
     hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send():release()
 end)
 
+-- URL test
+hs.urlevent.bind("someAlert", function(eventName, params)
+    hs.alert.show("Received someAlert")
+end)
+
 ---------------------
 --  Window Sizing  --
 ---------------------
@@ -257,21 +262,57 @@ hs.hotkey.bind({"cmd", "ctrl", "alt"}, "J", function()
   win:moveOneScreenSouth()
 end)
 
+
 ---------------------
 -- Pomodoro Timer  --
 ---------------------
 
--- Schedule Notification
-function startTimer()
-    hs.timer.doAfter(20 * 60, function()
-        hs.notify.new({
-            title="Pomodoro Complete",
-            informativeText="Did you complete your One Thing?"
-        }):send()
-    end)
-    hs.alert(" Pomodoro Timer Started ")
+-- Cancel Scheduled Notification
+function cancelTimer(silent)
+    if timer and timer:running() then
+        timer:stop()
+        if not silent then
+            hs.alert(" Pomodoro Canceled ")
+        end
+    else
+        if not silent then
+            hs.alert(" No Pomodoro Running ")
+        end
+    end
 end
-hs.hotkey.bind({"cmd", "ctrl"}, "T", startTimer)
+
+-- Schedule Notification
+function startTimer(work)
+    cancelTimer(true)
+
+    if work then
+        timer = hs.timer.doAfter(25 * 60, function()
+            hs.alert(" Pomodoro Complete ", 4)
+            hs.notify.new({
+                title="Pomodoro Complete",
+                informativeText="Did you complete your One Thing?"
+            }):send()
+        end)
+        hs.alert(" Pomodoro Timer Started ", 2)
+    else
+        timer = hs.timer.doAfter(5 * 60, function()
+            hs.alert(" Break Over ", 4)
+            hs.notify.new({
+                title="Pomodoro Break Over",
+                informativeText="Did you stretch? Look away? Enjoy your rest?"
+            }):send()
+        end)
+        hs.alert(" Break Started ", 2)
+    end
+end
+
+-- Simple Callbacks
+function startWork() startTimer(true) end
+function startBreak() startTimer(false) end
+
+hs.hotkey.bind({"cmd", "ctrl"}, "P", startWork)
+hs.hotkey.bind({"cmd", "ctrl", "shift"}, "P", startBreak)
+hs.hotkey.bind({"cmd", "ctrl", "alt"}, "P", cancelTimer)
 
 
 ------------------------
