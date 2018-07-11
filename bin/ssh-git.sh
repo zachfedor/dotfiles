898e7c2d8e6527a03029a0762fbe1ff7c16375a4
zachfedor <zachfedor@gmail.com>
2018-07-11 12:03:04 -0400

    Fixes git-ssh config

--- bin/ssh-git.sh
+++ bin/ssh-git.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$PKEY" ]; then
     # if PKEY is not specified, run ssh using default keyfile
-    ssh -i ~/.ssh/id_rsa-personal "$@"
+    ssh -i ~/.ssh/id_rsa-by "$@"
 else
     ssh -i "$PKEY" "$@"
 fi
